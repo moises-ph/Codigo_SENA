dc994414baf95a2a551135c9655155d2d46e3677
moisesph <mois.mp8@gmail.com>
2022-05-06 20:28:00 -0500

    Bases de datos actividad terminada

--- Bases de datos/Actividad Consultas/consultas.sql
+++ Bases de datos/Actividad Consultas/consultas.sql
@@ -31,3 +31,77 @@ SELECT nombre, ROUND(precio) from producto;
 SELECT nombre, round(precio) from producto;
 
 /*11*/
+SELECT codigo_fabricante as 'Codigo del fabricante en producto' from producto;
+
+/*12*/
+SELECT distinct codigo_fabricante from producto;
+
+/*13*/
+SELECT nombre from fabricante order by nombre ASC;
+
+/*14*/
+SELECT nombre from fabricante order by nombre DESC;
+
+/*15*/
+SELECT nombre, precio from producto order by nombre ASC, precio DESC;
+
+/*16*/
+select * from fabricante order by nombre DESC limit 5;
+
+/*17*/
+select * from fabricante order by nombre DESC limit 4, 3;
+
+/*18*/
+select nombre, precio from producto order by precio ASC limit 1;
+
+/*19*/
+select nombre, precio from producto order by precio DESC limit 1;
+
+/*20*/
+select nombre from producto where codigo_fabricante=2;
+
+/*21*/
+select nombre from producto where precio/4238 <=125;
+
+/*22*/
+select nombre from producto where precio/4238 >=400;
+
+/*23*/
+select nombre from producto where not precio/4238 >=400;
+
+/*24*/
+select * from producto where precio/4238 >80 and precio/4328 <300;
+
+/*25*/
+select * from producto where precio/4238 >200 and codigo_fabricante=6;
+
+/*26*/
+select * from producto where codigo_fabricante=1 or codigo_fabricante=3 or codigo_fabricante=5;
+
+/*27*/
+select * from producto where codigo_fabricante in(1, 3, 5);
+
+/*28*/
+select nombre, precio*100 as 'Centimos' from producto;
+
+/*29*/
+select nombre from fabricante where (nombre like 'S%');
+
+/*30*/
+select nombre from fabricante where (nombre like '%e');
+ 
+/*31*/
+select nombre from fabricante where (nombre like '%w%');
+
+/*32*/
+select nombre from fabricante where (nombre like '__');
+
+/*33*/
+select nombre from producto where (nombre like '$portatil$');
+
+/*34*/
+select nombre from producto where (nombre like '$Monitor$') and precio/4238<215;
+
+/*35*/
+select nombre, precio from producto where precio/4238  >=180 order by precio DESC, nombre ASC;
+
