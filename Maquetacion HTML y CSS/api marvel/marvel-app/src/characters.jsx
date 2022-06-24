fc88ad265f009b6ca3a2a7e9f68151d5ad4d5a19
moisesph <mois.mp8@gmail.com>
2022-06-24 09:05:40 -0500

    Terminar, character

--- Maquetacion HTML y CSS/api marvel/marvel-app/src/characters.jsx
+++ Maquetacion HTML y CSS/api marvel/marvel-app/src/characters.jsx
@@ -4,26 +4,17 @@ import { useState } from 'react';
 function Characters(props) {
     const [output, setOut] = useState();
     const [stories_html, setStories] = useState();
+    const [sotires_query, setQuerySt] = useState();
 
     const consultar_esp = async (query) =>{
-        let stories;
-        let sotires_name;
-        let stories_elem;
         let data;
         await fetch(`http://gateway.marvel.com/v1/public/characters?name=hulk&ts=1000&apikey=c1877a4b5d3693afc04219bd658fe78c&hash=39a54346f077173d691ac33efe20ae09`)
             .then(res => res.json()).then(data_res => {
                 data = data_res.data.results[0];
-                console.log(data_res);
             });
-        stories = data.stories.items;
-        stories.map( async item =>{
-            await fetch(item.resourceURL + '?ts=1000&apikey=c1877a4b5d3693afc04219bd658fe78c&hash=39a54346f077173d691ac33efe20ae09')
-                .then(res => res.json()).then(data => stories_elem = data.data.results[0]);
-            setStories(<li>
-                <h4>{stories_elem.tittle}</h4>
-            </li>);
-        })
+        setQuerySt(data.stories.items);
         console.log(data);
+
         setOut(
             <>
             <a className='imagen-personaje' target='blank' href={data.urls[0].url}>
@@ -32,7 +23,7 @@ function Characters(props) {
             <div className='info'>
                 <h2 className='personaje'>{data.name}</h2>
                 <p className='descripcion'>{data.description}</p>
-                <ul>{stories_html}</ul>
+                <ul id='stories-li'>{stories_html}</ul>
             </div>
             </>
         );
